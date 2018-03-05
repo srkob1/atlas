87629b5f18ee9443d96a1acb9c6c0a2feb867bc6
Steff <stephanie.kobakian@gmail.com>
2018-03-05 15:24:09 +1100

    plotting south australia

--- R/Cartogram.R
+++ R/Cartogram.R
@@ -2,3 +2,116 @@
 
 library(eechidna)
 load("~/atlas/data/sa2Small.Rda")
+load("~/atlas/data/sa2_map.Rda")
+load("~/atlas/data/sa2_data.Rda")
+
+sa2_data %>% distinct(SA2_NAME16, .keep_all = T) %>% select(id, SA2_MAIN16:POPULATION)-> sa2info
+
+Vic <- list("Ballarat","Bendigo","Geelong","Hume","Latrobe","Melbourne - Inner East","Melbourne - Inner South","Melbourne - Inner","Melbourne - North East","Melbourne - North West","Melbourne - Outer East","Melbourne - South East","Melbourne - West","Mornington Peninsula","North West","Shepparton","Warrnambool and South West")
+
+sa2_data %>% filter(SA4_NAME16 %in% Vic) %>%
+  select(id, SA2_NAME16, SA4_NAME16, SA2_5DIG16, long_c, lat_c, AREASQKM16) %>%
+  distinct() -> Victoria
+
+#Tasmania
+library(plotly)
+library(eechidna)
+plot_tas <- sa2_data %>% filter(STE_NAME16=="Tasmania")
+centroids_tas <- centroids %>% filter(ste=="Tasmania")
+
+left_join(centroids_tas)
+tas_carto <- aec_carto_f(centroids_tas, polygon.vertex=6, 
+                         density=100, dist.ratio = 5)
+
+tas_carto <- left_join(tas_carto, 
+                       centroids_tas,
+                       by = c("region"="id"))
+tas_carto <- tas_carto %>% mutate(label = paste(gsub(" ", "\n", gsub(" - ", " ", name))))
+
+distance <- c()
+
+library(geosphere)
+for (i in 1:nrow(tas_carto)) {
+  distVincentyEllipsoid(
+    c(tas_carto$x[i], tas_carto$y[i]),
+    c(tas_carto$long_c[i], tas_carto$lat_c[i]),
+    a = 6378249.145,
+    b = 6356514.86955,
+    f = 1 / 293.465
+  ) -> distance[i]
+}
+
+tas_carto$distance <- distance
+
+
+#Edit Tas locations
+
+tas_carto$x[tas_carto$name=="King Island"] <- tas_carto$long_c[tas_carto$name=="King Island"] 
+tas_carto$y[tas_carto$name=="King Island"] <- tas_carto$lat_c[tas_carto$name=="King Island"] 
+
+tas_carto$x[tas_carto$name=="Flinders and Cape Barren Islands"] <- tas_carto$long_c[tas_carto$name=="Flinders and Cape Barren Islands"] 
+tas_carto$y[tas_carto$name=="Flinders and Cape Barren Islands"] <- tas_carto$lat_c[tas_carto$name=="Flinders and Cape Barren Islands"] 
+
+
+
+tas_carto$x[tas_carto$name=="Burnie - Ulverstone Region"] <- 146.0000
+tas_carto$y[tas_carto$name=="Burnie - Ulverstone Region"] <- -42.0000
+
+tas_carto$x[tas_carto$name=="Beauty Point - Beaconsfield"] <- 146.8800
+tas_carto$y[tas_carto$name=="Beauty Point - Beaconsfield"] <- -41.2000
+
+tas_carto$x[tas_carto$name=="Derwent Park - Lutana"] <- 147.514
+tas_carto$y[tas_carto$name=="Derwent Park - Lutana"] <- -41.889
+
+ggplot(data=plot_data) + 
+ # geom_path(aes(x=long, y=lat, group=group),
+  #          colour="grey50") +
+  geom_hex(data=tas_carto, aes(x=x, y=y), size=3, alpha=0.1,
+             colour="white", binwidth=c(0.5))+
+  geom_segment(data=tas_carto, aes(x=long_c, xend=x, y=lat_c, yend=y, color=distance))+
+  geom_point(data=tas_carto, aes(x=long_c, y=lat_c), colour="pink")+
+  geom_text(data=tas_carto, aes(label=label, x=x, y=y),size=2)+
+  coord_equal()
+
+
+
+
+p <- ggplot(data=plot_tas) +
+  geom_path(aes(x=long, y=lat, group=group),
+            colour="grey50") +
+  geom_point(data=tas_carto, aes(x=x, y=y, label=label), size=2, alpha=0.4,
+             colour="#f0027f") +
+  coord_equal()
+
+ggplotly(p)
+
+## hex bin points
+
+#South Australia
+centroids %>% filter(ste=="South Australia") ->centroids_sa
+
+sa_carto <- aec_carto_f(centroids_sa)
+sa_carto <- left_join(sa_carto, 
+                       centroids_sa,
+                       by = c("region"="id"))
+sa_carto <- sa_carto %>% mutate(label = paste(gsub(" ", "\n", gsub(" - ", " ", name))))
+
+p <- ggplot(data=sa2_data %>% filter(STE_NAME16=="South Australia")) + 
+  geom_path(aes(x=long, y=lat, group=group),
+            colour="grey50") +
+  geom_point(data=sa_carto, aes(x=x, y=y, label=label), size=2, alpha=0.4,
+             colour="#f0027f") +
+  coord_equal() 
+
+
+ggplotly(p)
+
+## hex bin points
+
+ggplot(data=sa2_data %>% filter(STE_NAME16=="South Australia")) + 
+   geom_path(aes(x=long, y=lat, group=group),
+            colour="grey50") +
+  geom_hex(data=sa_carto, aes(x=x, y=y), size=1, alpha=0.2,
+           colour="#f0027f", binwidth=c(0.5)) +
+  geom_text(data=sa_carto, aes(label=label, x=x, y=y),size=2)+
+  coord_equal()
---
+++ R/plotSA.R
@@ -0,0 +1,90 @@
+# plot subset
+
+library(sf)
+library(rgdal)
+library(rmapshaper)
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
+library(ggrepel)
+library(geogrid)
+library(RColorBrewer)
+
+
+load("~/atlas/data/sa2_data.Rda")
+load("~/atlas/data/sa2_map.Rda")
+load("~/atlas/data/sa2Small.Rda")
+
+saSPDF <- subset(sa2Small, STE_NAME16=="South Australia")
+
+
+#sa area names
+Y <- saSPDF %>% split(.@data$SA4_NAME16) %>%
+  map_df(., nrow) %>%
+  gather(., key = "number", value = "val") %>%
+  filter(val>0) %>% select(number) %>% as.vector()
+
+
+myPalette <- colorRampPalette(rev(brewer.pal(11, "Greens")))
+sc <- scale_fill_gradientn(colours = myPalette(100), limits=c(1, 1700000))
+
+# geogrid hex map simulations
+for (i in seq(50)){
+  
+  seed <- (4018 + i)
+  
+  ap_saSPDF <-
+    assign_polygons(
+      saSPDF,
+      calculate_grid(
+        shape = saSPDF,
+        learning_rate = 0.01,
+        grid_type = "hexagonal",
+        seed = seed
+      )
+    )
+  # make dataframe
+  ap_saSPDF@data$id = rownames(ap_saSPDF@data)
+  ap_saSPDF.points = fortify(ap_saSPDF, region = "id")
+  ap_saSPDF.df = merge(ap_saSPDF.points, ap_saSPDF@data, by = "id")
+  
+  
+  ap_saSPDF.df <- ap_saSPDF.df %>% mutate(label = paste(gsub(" ", "\n", SA2_NAME16)))
+  
+  
+  ap_saSPDF.df <- ap_saSPDF.df %>% rowwise %>% 
+    mutate(distance = distVincentyEllipsoid(c(CENTROIX,
+                                              CENTROIY),
+                                            c(V1,V2),
+                                            a=6378249.145, b=6356514.86955, f=1/293.465))
+  
+  
+  plot <- ggplot(ap_saSPDF.df) +
+    geom_polygon(aes(
+      x = long,
+      y = lat,
+      group = group,
+      fill = distance
+    )) +
+    geom_text(
+      aes(
+        V1,
+        V2,
+        label = label),
+      size = 3,
+      color = "black"
+    ) +
+    sc +
+    coord_equal() +
+    #guides(fill = FALSE) +
+    theme_void()  +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "transparent", colour = NA),
+      plot.background = element_rect(fill = "transparent", colour = NA)
+    )
+  plot
+  ggsave(paste0("South Australia", seed, ".png", sep=""), plot, bg = "transparent")
+  
+}
