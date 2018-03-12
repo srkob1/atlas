b5aec71402e6ffaeea972e03a79a0bd146613e46
Cancer Atlas <canceratlas@dyn-130-194-61-28.its.monash.edu.au>
2018-03-12 12:47:12 +1100

    New south wales plots

---
+++ R/plotNSW.R
@@ -0,0 +1,94 @@
+# plot subset
+
+library(sf)
+library(rgdal)
+library(rmapshaper)
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
+library(ggrepel)
+library(geogrid)
+library(RColorBrewer)
+library(geosphere)
+
+load("~/atlas/data/sa2_data.Rda")
+load("~/atlas/data/sa2_map.Rda")
+load("~/atlas/data/sa2Small.Rda")
+
+nswSPDF <- subset(sa2Small, STE_NAME16=="New South Wales")
+
+
+#sa area names
+Y <- nswSPDF %>% split(.@data$SA4_NAME16) %>%
+  map_df(., nrow) %>%
+  gather(., key = "number", value = "val") %>%
+  filter(val>0) %>% select(number) %>% as.vector()
+
+
+myPalette <- colorRampPalette(rev(brewer.pal(11, "Greens")))
+sc <- scale_fill_gradientn(colours = myPalette(100), limits=c(1, 1700000))
+
+distanceList <- list()
+# geogrid hex map simulations
+for (i in seq(50:150)){
+  
+  seed <- (4018 + i)
+  
+  ap_nswSPDF <-
+    assign_polygons(
+      nswSPDF,
+      calculate_grid(
+        shape = nswSPDF,
+        learning_rate = 0.01,
+        grid_type = "hexagonal",
+        seed = seed
+      )
+    )
+  # make dataframe
+  ap_nswSPDF@data$id = rownames(ap_nswSPDF@data)
+  ap_nswSPDF.points = fortify(ap_nswSPDF, region = "id")
+  ap_nswSPDF.df = merge(ap_nswSPDF.points, ap_nswSPDF@data, by = "id")
+  
+  
+  ap_nswSPDF.df <- ap_nswSPDF.df %>% mutate(label = paste(gsub(" ", "\n", gsub(" - ", " ", SA2_NAME16))))
+  
+  
+  ap_nswSPDF.df <- ap_nswSPDF.df %>% rowwise %>% 
+    mutate(distance = distVincentyEllipsoid(c(CENTROIX,
+                                              CENTROIY),
+                                            c(V1,V2),
+                                            a=6378249.145, b=6356514.86955, f=1/293.465))
+  
+  distanceList[[i]] <- ap_nswSPDF.df %>% select(id, SA2_NAME16,
+                                               CENTROIX,
+                                               CENTROIY, V1,V2, distance)
+  
+  plot <- ggplot(ap_nswSPDF.df) +
+    geom_polygon(aes(
+      x = long,
+      y = lat,
+      group = group,
+      fill = distance
+    )) +
+    geom_text(
+      aes(
+        V1,
+        V2,
+        label = label),
+      size = 3,
+      color = "black"
+    ) +
+    sc +
+    coord_equal() +
+    #guides(fill = FALSE) +
+    theme_void()  +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "transparent", colour = NA),
+      plot.background = element_rect(fill = "transparent", colour = NA)
+    )
+  plot
+  ggsave(paste0("New South Wales", seed, ".png", sep=""), plot, bg = "transparent")
+  
+}
