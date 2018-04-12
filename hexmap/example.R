3dd3b84ee29180cbc323edc69286a7f50bb8c48a
Steff <stephanie.kobakian@gmail.com>
2018-04-13 02:59:26 +1000

    check population ordering works

--- hexmap/example.R
+++ hexmap/example.R
@@ -26,14 +26,18 @@ centroid_data <- NULL
 for (i in 1:length(ausSPDF@polygons)) {
    centroid_data$long[i] <- ausSPDF@polygons[[i]]@labpt[1]
    centroid_data$lat[i] <- ausSPDF@polygons[[i]]@labpt[2]
-   centroid_data$SA2_NAME16[i] <- ausSPDF@data$SA2_NAME16[which(ausSPDF@data$id == ausSPDF@polygons[[i]]@ID)]
+   centroid_data$SA2_NAME16[i] <- ausSPDF@data$SA2_NAME16[which(ausSPDF@data$id == ausSPDF@polygons[[i]]@ID)] 
+   centroid_data$population[i] <- ausSPDF@data$population[which(ausSPDF@data$id == ausSPDF@polygons[[i]]@ID)]
 }
 
 # Arrange grid data
 grid <- create_grid(ausSPDF@bbox, radius) %>%
   cbind(., id = 1:NROW(.), assigned = rep(FALSE, NROW(.)))
 
-aus_centroids <- assign_hexagons(centroid_data, grid)
+# order centroids for allocation
+map_dfc(centroid_data, unlist) %>%
+  arrange(desc(population)) %>%
+  bind_cols(., assign_hexagons(centroid_data, grid)) -> aus_centroids
 
 # Data needs to be output
 # csv
@@ -51,10 +55,10 @@ save(aus_centroids, file = "aus_centroids.Rda")
   geom_polygon(aes(x=long, y=lat, group=group, fill=population, label=SA2_NAME16))
   )
 
-(hexmap <- ggplot(ausGridAllocations) +
-  stat_summary_hex(aes(x = hex_long, y = hex_lat, z = population, label=SA2_NAME16),
-                   alpha = 0.75, binwidth = 0.23)
-  )
-
+(hexmap <-ggplot(aus_centroids) +
+  geom_polygon(data= sa2_map,aes(x=long, y=lat, group=group, fill=population, label=SA2_NAME16))+
+geom_hex(aes(x = hex_long, y = hex_lat, fill = population, label=SA2_NAME16),
+           stat = "identity", colour = NA, alpha = 0.75)
+)
 # Interactive plot code
 
