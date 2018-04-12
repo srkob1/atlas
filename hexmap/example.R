1adbcaa3a67a70901b8eeab64401a5a305050d3a
Steff <stephanie.kobakian@gmail.com>
2018-04-13 00:25:23 +1000

    Included data creation (two data frames as inputs to assign_hexagons function

--- hexmap/example.R
+++ hexmap/example.R
@@ -1,12 +1,60 @@
 # This file has the code to actually create the Oz hexmap
-# Australia grid
+# Australia 
+library(tidyverse)
+library(geosphere)
+library(hexbin)
+
+load("data/sa2Small.Rda")
+load("data/sa2_map.Rda")
+
+# Find the functions to use
+source("hexmap/hexagon.R")
+
+# subset to remove Islands
+ausSPDF <- subset(sa2Small, (STE_NAME16!="Other Territories"))
+ausSPDF <- subset(ausSPDF, (SA2_NAME16!="Lord Howe Island"))
+
+sa2_map <- sa2_map %>% 
+  filter(STE_NAME16!="Other Territories") %>%
+  filter(SA2_NAME16!="Lord Howe Island")
+
+# set radius for Australian map
+radius <- 0.3
+
+# Arrange original centroid data
+centroid_data <- NULL
+for (i in 1:length(ausSPDF@polygons)) {
+   centroid_data$long[i] <- ausSPDF@polygons[[i]]@labpt[1]
+   centroid_data$lat[i] <- ausSPDF@polygons[[i]]@labpt[2]
+   centroid_data$SA2_NAME16[i] <- ausSPDF@data$SA2_NAME16[which(ausSPDF@data$id == ausSPDF@polygons[[i]]@ID)]
+}
+
+# Arrange grid data
+grid <- create_grid(ausSPDF@bbox, radius) %>%
+  cbind(., id = 1:NROW(.), assigned = rep(FALSE, NROW(.)))
+
+aus_centroids <- assign_hexagons(centroid_data, grid)
 
 # Data needs to be output
 # csv
+write_csv(aus_centroids, path = "aus_centroids.csv")
 # and rda for us
+save(aus_centroids, file = "aus_centroids.Rda")
+
+
 
 # Plotting code
-# Points and hexes: maybe this works http://unconj.ca/blog/custom-hexbin-functions-with-ggplot.html
+# Points and hexes: inspired by http://unconj.ca/blog/custom-hexbin-functions-with-ggplot.html
 # colour by population
 
+(geomap <- ggplot(sa2_map) +
+  geom_polygon(aes(x=long, y=lat, group=group, fill=population, label=SA2_NAME16))
+  )
+
+(hexmap <- ggplot(ausGridAllocations) +
+  stat_summary_hex(aes(x = hex_long, y = hex_lat, z = population, label=SA2_NAME16),
+                   alpha = 0.75, binwidth = 0.23)
+  )
+
 # Interactive plot code
+
--- hexmap/hexagon.R
+++ hexmap/hexagon.R
@@ -10,13 +10,13 @@ create_grid <- function(bbox, radius) {
                                    radius))
 
   # Only shift every second latitude - to make hex structure
-  # DI SAYS: check if this creates a vector
     lat <- grid %>% select(lat) %>%
       distinct() %>%
       filter(row_number() %% 2 == 1) %>% unlist()
 
   grid <- grid %>% rowwise %>%
     mutate(long = ifelse(lat %in% lat, long, long + (radius/2)))
+  
   return(grid)
 }
 
@@ -24,16 +24,21 @@ create_grid <- function(bbox, radius) {
 # and assign to closest hexgrid location.
 # DI SAYS: change separate long, lat input to one tibble for
 #         centroids, and one for hexgrid
-assign_hexagons <- function(long_c, lat_c, hex_long, hex_lat) {
+# STEFF SAYS: what about using only the shape file, create grid internally?
+assign_hexagons <- function(centroid_data, grid_data) {
   # consider each hexagon
   hexVec <- NULL
+  hex_centroids <- NULL
+  
+  long_c <- centroid_data$long 
+  lat_c <- centroid_data$lat
 
   # check for equal length
   stopifnot(length(long_c) ==length(lat_c))
 
   # create a tibble of all available grid points to allocate an area to
-  grid <- cbind(hex_long, hex_lat) %>% as.tibble %>%
-    mutate(id = 1:length(hex_long),
+  grid <- grid_data %>% as.tibble %>%
+    mutate(id = 1:length(grid_data$long),
            assigned = ifelse(id %in% hexVec, TRUE, FALSE))
 
   # Loop over spatial polygon centroids to assign to nearest hexgrid
@@ -41,19 +46,19 @@ assign_hexagons <- function(long_c, lat_c, hex_long, hex_lat) {
     print(i)
     olong <- long_c[i]
     olat <- lat_c[i]
-
+    
     grid_nasgn <- grid %>% filter(!assigned)
-
+    
     distance <- distVincentyEllipsoid(
       c(olong, olat), grid_nasgn,
       a=6378249.145, b=6356514.86955, f=1/293.465)
-    mindist_id <- cbind(grid_nasgn, distance) %>%
+    distance_df <- cbind(grid_nasgn, distance) %>%
       arrange(distance)
-
-    id <- mindist_id$id[1]
-    grid$assigned[id] <- TRUE
-    hexVec <- c(hexVec, id)
-    new_centroid <- mindist_id[1,]
+    
+    mindist_id <- distance_df$id[1]
+    grid$assigned[mindist_id] <- TRUE
+    hexVec <- c(hexVec, mindist_id)
+    new_centroid <- distance_df[1,]
     hex_centroids <- bind_rows(hex_centroids, new_centroid)
   }
 
