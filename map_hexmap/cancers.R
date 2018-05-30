39dd4c942a47b40bae55f6bc8a014f824130e342
Dianne Cook <visnut@gmail.com>
2018-05-30 10:34:57 +1000

    Stomach cancer added

--- map_hexmap/cancers.R
+++ map_hexmap/cancers.R
@@ -28,8 +28,11 @@ sa2_estimates <- sa2_estimates %>%
   mutate(cancergrp = as.factor(cancergrp),
          sa2 = as.factor(sa2))
 
-sa2_estimates %>% count(cancergrp)
+cancers <- sa2_estimates %>% count(cancergrp)
+sa2_info <- sa2_info %>% mutate(cancergrp = as.factor(cancergrp))
+cancers <- inner_join(cancers, sa2_info[,1:2], by="cancergrp") %>% distinct()
 
+# Test for oesophageal cancer
 cr11 <- sa2_estimates %>%
   filter(cancergrp == 11) %>%
   select(cancergrp, sa2, persons_p50)
@@ -70,4 +73,52 @@ ggplot() +
   guides(fill=FALSE)
 ggsave("Oesophageal_map.png")
 
-#
+# loop over cancers
+for (i in 3:nrow(cancers)) {
+  cat(i, "\n")
+  cr <- sa2_estimates %>%
+    filter(cancergrp == cancers$cancergrp[i]) %>%
+    select(cancergrp, sa2, persons_p50)
+  # Check match - there is one mismatch
+  cr %>% anti_join(sa2_hex, by=c("sa2"="SA2_5DIG11"))
+
+  # Now join, using hexes as base
+  cr_hex <- left_join(sa2_hex %>% select(SA2_5DIG11, hex_long, hex_lat, long_c, lat_c, SA2_NAME11, GCC_NAME11, STE_NAME11, SUA), cr, by=c("SA2_5DIG11"="sa2"))
+
+  cr_hex <- cr_hex %>% mutate(colrs = case_when(
+    persons_p50 < 0.75  ~ "A",
+    persons_p50 >= 0.75 & persons_p50 < 1  ~ "B",
+    persons_p50 >= 1 & persons_p50 < 1.25  ~ "C",
+    persons_p50 >= 1.25 & persons_p50 < 1.5  ~ "D",
+    persons_p50 > 1.5  ~ "E")) %>%
+    mutate(colrs = factor(colrs, levels = c("A", "B", "C", "D", "E")))
+
+  # Hexmap
+  p1 <- ggplot() + geom_polypath(data=aus_map, aes(x=long, y=lat, group=group),
+                           fill="grey98", colour="white", size=1) +
+    geom_hex(data=cr_hex, aes(x = hex_long, y = hex_lat,
+                                fill = colrs),
+             colour = NA, stat = "identity") +
+    scale_fill_manual(values = nac_colours) +
+    theme_map() +
+    guides(fill=FALSE)
+  flnm <- paste0(cancers$cancergrpname[i], "_hex.png")
+  flnm <- sub(" cancer", "", flnm)
+  ggsave(flnm, plot=p1)
+
+  # Choropleth map
+  sa2_map <- left_join(sa2_tidy, cr_hex, by="SA2_5DIG11")
+  p2 <- ggplot() +
+    geom_polygon(data=sa2_map, aes(x=long, y=lat, group=group,
+                                   fill = colrs),
+                 colour="white", size=0.1) +
+    #expand_limits(x = sa2_map$long, y = sa2_map$lat) +
+    scale_fill_manual(values = nac_colours) +
+    theme_map() +
+    guides(fill=FALSE)
+  flnm <- paste0(cancers$cancergrpname[i], "_map.png")
+  flnm <- sub(" cancer", "", flnm)
+  ggsave(flnm, plot=p2)
+
+}
+
